abb8efe848f57c90581663676c443bd307d703f0
Semyon Belokovsky <igrampe@gmail.com>
2016-04-14 17:46:30 +0500

    Initial Commit

---
+++ SkyEngTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SkyEngTest
+//
+//  Created by Semyon Belokovsky on 14/04/16.
+//  Copyright © 2016 igrampe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SkyEngTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SkyEngTest
+//
+//  Created by Semyon Belokovsky on 14/04/16.
+//  Copyright © 2016 igrampe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
